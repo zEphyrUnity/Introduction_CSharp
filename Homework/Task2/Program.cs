7d6c9c64ffe82a9cb9644c97fba74f9ad90165f7
zEphyr <stelzz@ya.ru>
2023-06-11 11:24:06 +0300

    Task2 version 1.0

--- Homework/Task2/Program.cs
+++ Homework/Task2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пожалуйста два числа для сравнения");
+
+int a = TakeUserNum();
+int b = TakeUserNum();
+
+if(NumComparison(a, b))
+{
+    Console.WriteLine($"a = {a}; b = {b} -> max = {a}");
+}
+else
+{
+    Console.WriteLine($"a = {a}; b = {b} -> max = {b}");
+}
+
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum))
+    {
+        Console.WriteLine("Пожалуйста введите число!");
+    }
+
+    return userNum;
+}
+
+bool NumComparison(int a, int b)
+{
+    if(a > b) 
+        return true;
+    else 
+        return false;
+}
