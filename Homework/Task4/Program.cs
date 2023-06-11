cdadbfd45c371e6487b15379083d2be0502ebcf2
zEphyr <stelzz@ya.ru>
2023-06-11 16:58:06 +0300

    Task4 version 1.0

--- Homework/Task4/Program.cs
+++ Homework/Task4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пожалуйста три числа для сравнения");
+
+int a = TakeUserNum();
+int b = TakeUserNum();
+int c = TakeUserNum();
+int max = NumComparison(a, b, c);
+
+Console.WriteLine($"{a}, {b}, {c} -> {max}");
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum))
+    {
+        Console.WriteLine("Пожалуйста введите число!");
+    }
+
+    return userNum;
+}
+
+int NumComparison(int a, int b, int c)
+{
+    int max = a;
+
+    if(max < b) max = b;
+    if(max < c) max = c;
+
+    return max;
+}
