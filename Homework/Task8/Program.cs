655329236f96df03e5833939e6d66d15861bb997
zEphyr <stelzz@ya.ru>
2023-06-12 22:27:29 +0300

    Task8 version 1.0

--- Homework/Task8/Program.cs
+++ Homework/Task8/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число, для получения ряда четных чисел от 1 до N");
+int userNum = TakeUserNum();
+
+Console.Write($"{userNum} -> 2");
+for(int i = 4; i <= userNum; i += 2)
+{
+    Console.Write($", {i}");
+}
+
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum) || userNum < 2)
+    {
+        Console.WriteLine("Пожалуйста введите число!");
+    }
+
+    return userNum;
+}
