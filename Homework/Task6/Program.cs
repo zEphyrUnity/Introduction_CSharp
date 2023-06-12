9ad040ad0e5708e87534508e85bdcaf544d7a3ef
zEphyr <stelzz@ya.ru>
2023-06-12 21:57:45 +0300

    Task6 version 1.0

--- Homework/Task6/Program.cs
+++ Homework/Task6/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число, для проверки на чётность");
+int userNum = TakeUserNum();
+bool isEven = IsEvenNumber(userNum);
+
+if(isEven)
+    Console.WriteLine($"{userNum} -> да");
+else
+    Console.WriteLine($"{userNum} -> нет");
+
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum))
+    {
+        Console.WriteLine("Пожалуйста введите число!");
+    }
+
+    return userNum;
+}
+
+bool IsEvenNumber(int userNum)
+{
+    if(userNum % 2 == 0) 
+        return true;
+    else 
+        return false;
+}
