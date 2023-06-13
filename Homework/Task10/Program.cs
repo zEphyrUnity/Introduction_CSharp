b2f19402d91929e9a516ac2da0c3b32d528f0df9
zEphyr <stelzz@ya.ru>
2023-06-13 18:48:42 +0300

    Task10 version 1.0

--- Homework/Task10/Program.cs
+++ Homework/Task10/Program.cs
@@ -2,3 +2,21 @@
 // вход трёхзначное число и на выходе показывает вторую 
 // цифру этого числа.
 
+Console.Clear();
+Console.WriteLine("Введите пожалуйста трёхзначное число");
+
+int value = TakeUserNum();
+Console.WriteLine((value / 10) % 10);
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum) | (userNum < 100 | userNum > 999))
+    {
+        Console.WriteLine("Пожалуйста введите трёхзначное число!");
+    }
+
+    return userNum;
+}
+
