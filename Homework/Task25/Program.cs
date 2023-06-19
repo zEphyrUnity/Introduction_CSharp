0c7d94ac816d5dc0768eb30b25298a512e45ed6a
zEphyr <stelzz@ya.ru>
2023-06-19 14:30:16 +0300

    Создана инфраструктура проекта Task25

---
+++ Homework/Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum))
+        Console.WriteLine("Пожалуйста введите число!");
+
+    return userNum;
+}
