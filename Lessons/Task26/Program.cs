babf8279b0bfe887991f91cb8e1fe340d6a53c0d
zEphyr <stelzz@ya.ru>
2023-06-20 18:40:20 +0300

    Добавил задания из семинара 4

---
+++ Lessons/Task26/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 26: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int userNum = TakeUserNum();
+
+Console.WriteLine(CountNum(userNum));
+
+
+int CountNum(int userNum)
+{
+    int i = 0;
+
+    while(userNum > 1)
+    {
+        userNum /= 10;
+        i++;
+    }
+
+    return i;
+}
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum))
+        Console.WriteLine("Пожалуйста введите число!");
+
+    return userNum;
+}
---
+++ Lessons/Task30/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 30: Напишите программу, которая 
+// выводит массив из 8 элементов, заполненный 
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+const int ARRAYSIZE = 8;
+int[] randomBinaryArray = RandomBinaryArray();
+PrintArray(randomBinaryArray);
+
+int[] RandomBinaryArray()
+{
+    int[] array = new int[ARRAYSIZE];
+    Random randomName = new Random();
+
+    for(int i = 0; i < ARRAYSIZE; i++)
+        array[i] = randomName.Next(0, 2);
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write($"{element} ");
+
+    Console.WriteLine();
+}
