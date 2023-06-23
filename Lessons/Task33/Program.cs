573e454f59a24e69b49e527b22d3f1cf58368103
zEphyr <stelzz@ya.ru>
2023-06-23 14:34:17 +0300

    Создана новая ветка homework, добавлены задачи из семинара

---
+++ Lessons/Task33/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+Console.WriteLine("Введите число");
+int userNumber = TakeUserNum();
+int[] array = RandomArray();
+PrintArray(array);
+
+if(FindUserNum(userNumber, array))
+    Console.WriteLine($"Число {userNumber} содержится в массиве");
+else
+    Console.WriteLine($"Число {userNumber} Не содержится в массиве");
+
+
+bool FindUserNum(int userNumber, int[] array)
+{
+    bool isFind = false;
+
+    foreach(var element in array)
+    {
+        if(element == userNumber)
+            isFind = true;
+    }
+
+    return isFind;
+}
+
+int[] RandomArray()
+{
+    int[] array = new int[10];
+    Random random = new Random();
+
+    for(int i = 0; i < array.Length; i++)
+        array[i] = random.Next(-5, 5);
+
+    return array;
+}
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum))
+        Console.WriteLine("Пожалуйста введите число!");
+
+    return userNum;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write($"{element} ");
+
+    Console.WriteLine();
+}
---
+++ Lessons/Task37/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] array = RandomArray();
+int[] arrayProduct = ArrayProduct(array);
+
+PrintArray(array);
+PrintArray(arrayProduct);
+
+int[] ArrayProduct(int[] array)
+{
+    int[] arrayProduct = new int[(array.Length / 2) + 1];
+
+    for(int i = 0; i < (array.Length / 2) + 1; i++)
+    {
+        if(i == (array.Length / 2))
+            arrayProduct[i] = array[i];
+        else
+            arrayProduct[i] = array[i] * array[(array.Length - 1) - i];
+    }
+        
+    return arrayProduct;
+}
+
+int[] RandomArray()
+{
+    int[] array = new int[5];
+    Random random = new Random();
+
+    for(int i = 0; i < array.Length; i++)
+        array[i] = random.Next(-10, 11);
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write($"{element} ");
+
+    Console.WriteLine();
+}
